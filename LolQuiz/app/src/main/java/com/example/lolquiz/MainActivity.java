9fc7741478a5209c9b676a9259b8a55ac7c3071c
AlvaroGA99 <55707621+AlvaroGA99@users.noreply.github.com>
2020-10-25 21:42:17 +0100

    Flujo de Actividades

--- LolQuiz/app/src/main/java/com/example/lolquiz/MainActivity.java
+++ LolQuiz/app/src/main/java/com/example/lolquiz/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.lolquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    Button optionsButton =  (Button)  findViewById(R.id.optionsButton);
+    Button newGameButton  = (Button)  findViewById(R.id.newGameButton);
+    Button rankingButton = (Button)  findViewById(R.id.rankingButton);
 
+
+    public void onClickNewGameButton(View v){
+
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void onClickOptionsActivity(View v){
+
+        Intent intent = new Intent(this, OptionsActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void onClickRankingButton(View v){
+
+        Intent intent = new Intent(this, RankingActivity.class);
+        startActivity(intent);
+
+    }
 }
