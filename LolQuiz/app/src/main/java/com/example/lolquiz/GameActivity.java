090f7e40bcb52536df7baf0a3f8d091f3d8bf77b
rodron <alexsp2399@gmail.com>
2020-10-28 23:58:00 +0100

    Retoques diseño

--- LolQuiz/app/src/main/java/com/example/lolquiz/GameActivity.java
+++ LolQuiz/app/src/main/java/com/example/lolquiz/GameActivity.java
@@ -4,12 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        InputStream is = getResources().openRawResource(R.raw.preguntas);
+        ArrayList<String[]> questions = new ArrayList<String[]>();
+
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line;
+            String aux[] = new String[6];
+            for (int i = 0; (line = br.readLine()) != null; i++) {
+                aux[i%6] = line;
+                if(i%6 == 0 && i/6>0){
+                    questions.add(aux);
+                }
+            }
+            br.close();
+        }
+        catch (IOException e) {
+            //You'll need to add proper error handling here
+        }
+
     }
 
 
--- LolQuiz/app/src/main/java/com/example/lolquiz/MainActivity.java
+++ LolQuiz/app/src/main/java/com/example/lolquiz/MainActivity.java
@@ -7,35 +7,32 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button newGameButton =  (Button)  findViewById(R.id.newGameButton);
+        newGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newGame();
+            }
+        });
+        //Button rankingButton = (Button)  findViewById(R.id.rankingButton);
     }
 
-    /*Button optionsButton =  (Button)  findViewById(R.id.optionsButton);
-    Button newGameButton  = (Button)  findViewById(R.id.newGameButton);
-    Button rankingButton = (Button)  findViewById(R.id.rankingButton);
-
-
-    public void onClickNewGameButton(View v){
+    public void newGame(){
 
         Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
 
     }
 
-    public void onClickOptionsActivity(View v){
-
-        Intent intent = new Intent(this, OptionsActivity.class);
-        startActivity(intent);
-
-    }
-
+/*
     public void onClickRankingButton(View v){
-
         Intent intent = new Intent(this, RankingActivity.class);
         startActivity(intent);
 
