2ac7ea292750b0333bdcaa0097a784e08617dce0
Rodron <alexsp2399@gmail.com>
2020-11-20 21:50:23 +0100

    Cambios en el menú de opciones (incompleto)

--- LolQuiz/app/src/main/java/com/example/lolquiz/OptionsActivity.java
+++ LolQuiz/app/src/main/java/com/example/lolquiz/OptionsActivity.java
@@ -7,16 +7,55 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class OptionsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    int questNumber = 20;
+    int categCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+
+        CheckBox [] categs = new CheckBox[4];
+
+
+
+        categs[0] = (CheckBox) findViewById(R.id.category1);
+        categs[1] = (CheckBox) findViewById(R.id.category2);
+        categs[2] = (CheckBox) findViewById(R.id.category2);
+        categs[3] = (CheckBox) findViewById(R.id.category3);
+
+        for(int i = 0; i < 4; i++) {
+
+            categs[i].setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (isChecked){
+                        checkAdd(1);
+                    }
+                    else {
+                        checkAdd(-1);
+                    }
+                }
+            });
+
+            categs[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (categCount == 0){
+                        ((CheckBox)v).setChecked(true);
+                    }
+                    //Toast.makeText(((CheckBox)v).getContext(), "" + categCount, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
 
         ImageButton back = (ImageButton) findViewById(R.id.back);
         back.setOnClickListener(new View.OnClickListener(){
@@ -26,27 +65,60 @@ public class OptionsActivity extends AppCompatActivity implements AdapterView.On
             }
         });
 
-        //Configuración de los parámetros del spinner
-        Spinner spinner = findViewById(R.id.spinnerTheme);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.themes, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
+        TextView questions = (TextView) findViewById(R.id.questions);
+        questions.setText("" + questNumber);
+
+        ImageButton addQuestion = (ImageButton) findViewById(R.id.addQuestion);
+        addQuestion.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                categs[0].setChecked(true);
+                if (questNumber<20){
+                    questNumber++;
+                    questions.setText("" + questNumber);
+                }
+            }
+        });
+
+        ImageButton subsQuestion = (ImageButton) findViewById(R.id.subsQuestion);
+        subsQuestion.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if (questNumber>5) {
+                    questNumber--;
+                    questions.setText("" + questNumber);
+                }
+            }
+        });
+    }
+
+    public int getCategsCheck(CheckBox[] categs){
+        int count = 0;
+        for(CheckBox check : categs){
+            if (check.isChecked()){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public void checkAdd(int n){
+        categCount += n;
+    }
+
+
+    public void goToMenu(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String text = parent.getItemAtPosition(position).toString();
-        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
-    }
-
-    public void goToMenu(){
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
     }
 }
